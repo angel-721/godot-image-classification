a8e38858092fd873547c92f58d7769b5fb6c57e7
samhunt404 <samuelhunt404@gmail.com>
2023-11-17 10:50:42 -0700

    Added Screenshoting

--- Player.gd
+++ Player.gd
@@ -6,13 +6,18 @@ export(float) var mouseSpeed
 export(float) var grav
 
 onready var cam = $Camera
+onready var viewport = $Viewport
 
 var actualVelocity := Vector3.ZERO
 
+var texture := ViewportTexture.new()
+
+var numScreenshots : int = 0
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	texture = viewport.get_texture()
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	var dir = Input.get_vector("Walk_Left","Walk_Right","Walk_Forward","Walk_Back")
 	
 	var vel = Vector3(dir.x * speed, 0,dir.y * speed)
@@ -33,3 +38,9 @@ func _unhandled_input(event):
 		cam.rotation.x = clamp(cam.rotation.x,deg2rad(-70),deg2rad(70))
 	if event.is_action_pressed("Quit"):
 		get_tree().quit()
+	if event.is_action_pressed("TakePic"):
+		_take_picture()
+		
+func _take_picture():
+	numScreenshots += 1
+	texture.get_data().save_png("res://SavedPics/Screenshot" + String(numScreenshots) + ".png")
