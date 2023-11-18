466d799935a8cb1f363485c9f524580da6c859ca
angel-721 <angelvelasquez2002@gmail.com>
2023-11-18 05:36:38 -0700

    Refactor Screenshot script

--- Scripts/ScreenshotHandler.gd
+++ Scripts/ScreenshotHandler.gd
@@ -2,10 +2,8 @@ extends Node
 
 
 var currScreenshot : Image
-var imgName = "Screenshot"
+var imgName := "Screenshot"
 
-func _ready():
-	pass
 
 func _setScreenshot(newScreenShot : Image):
 	currScreenshot = newScreenShot
@@ -25,39 +23,39 @@ func getScreenImgRGBArray() -> PoolVector3Array:
 
 #Save image to file
 func saveScreenshot(path : String) -> int:
-	var dir = Directory.new()
+	var dir := Directory.new()
 	var imgCount : int
 	if(not dir.dir_exists(path)):
-		var err = dir.make_dir(path)
+		var err := dir.make_dir(path)
 		if(err != 0):
 			return err
 		imgCount = 0
 	elif(dir.open(path) == OK):
 		dir.list_dir_begin(true)
-		var currFile = dir.get_next()
+		var currFile := dir.get_next()
 		while(currFile):
 			print(currFile)
 			if(currFile.ends_with(".png")):
 				imgCount += 1
 			currFile = dir.get_next()
 		print(currFile)
-	var val = currScreenshot.save_png(path + imgName + str(imgCount + 1) + ".png")
+	var val := currScreenshot.save_png(path + imgName + str(imgCount + 1) + ".png")
 	return val
 
 func saveScreenshotOne(path : String) -> int:
-	var dir = Directory.new()
+	var dir := Directory.new()
 	var imgCount : int = 0
 	if(not dir.dir_exists(path)):
-		var err = dir.make_dir(path)
+		var err := dir.make_dir(path)
 		if(err != 0):
 			return err
 		imgCount = 0
 	elif(dir.open(path) == OK):
 		dir.list_dir_begin(true)
-		var currFile = dir.get_next()
+		var currFile := dir.get_next()
 		while(currFile):
 			if(currFile.ends_with(".png")):
 				imgCount += 1
 			currFile = dir.get_next()
-	var val = currScreenshot.save_png(path + "read" + ".png")
+	var val := currScreenshot.save_png(path + "read" + ".png")
 	return val
